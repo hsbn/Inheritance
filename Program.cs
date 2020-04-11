e76a4294e26a02e33d190b341356fcb7f1819c78
habib <61862102+hsbn@users.noreply.github.com>
2020-04-11 20:58:09 +0700

    commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Habib", 20);
+            person.GetNameAndAge();
+            Console.WriteLine();
+            Teacher teacher = new Teacher("Kamarudin", 35, "190302", "Pemrograman");
+            teacher.GetNameAndAge();
+            Console.WriteLine();
+            Student student = new Student("Habib", 20, "19.11.2636", "habib.h@students.amikom.ac.id");
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+        }
+    }
+}
